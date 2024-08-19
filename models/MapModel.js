9ed245dfc90011f0c9d0b2fa0c87647cd2627ef1
shahyad <shahyad.hack@gmail.com>
2024-08-19 12:27:29 +0330

    add new field

--- models/MapModel.js
+++ models/MapModel.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
 const schema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   lat: {
     type: Number,
     required: true,
   },
   lng: {
+    type: Number,
+    required: true,
+  },
+  frequency: {
     type: Number,
     required: true,
   },
