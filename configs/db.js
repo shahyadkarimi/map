1ca1c6349c6fea7d06b5cd7f7d6c1b9fc2e5b19c
shahyad <shahyad.hack@gmail.com>
2024-08-20 15:05:23 +0330

    fix

--- configs/db.js
+++ configs/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     if (mongoose.connections[0].readyState) {
+      console.log(mongoose.connections[0]);
       return true;
     } else {
       await mongoose.connect(process.env.MONGO_URL);
