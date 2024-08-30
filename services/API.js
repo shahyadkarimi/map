3a2a09e8be28ee4234a803a3330b7ef609a99849
shahyad <shahyad.hack@gmail.com>
2024-08-30 21:10:34 +0330

    commit

--- services/API.js
+++ services/API.js
@@ -42,36 +42,6 @@ const getData = async (param, data, withToken = false) => {
   return res;
 };
 
-// patch method
-const patchData = async (param, data, withToken = false) => {
-  if (withToken) {
-    let token = localStorage.getItem("token");
-    const res = await servicesApi.patch(param, data, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-
-    return res;
-  }
-
-  const res = await servicesApi.patch(param, data);
-  return res;
-};
-
-// patch method
-const putData = async (param, data, withToken = false) => {
-  if (withToken) {
-    let token = localStorage.getItem("token");
-    const res = await servicesApi.put(param, data, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-
-    return res;
-  }
-
-  const res = await servicesApi.put(param, data);
-  return res;
-};
-
 // delete method
 const deleteData = async (param, data, withToken = false) => {
   if (withToken) {
@@ -88,4 +58,4 @@ const deleteData = async (param, data, withToken = false) => {
   return res;
 };
 
-export { postData, getData, patchData, deleteData, putData };
+export { postData, getData, deleteData};
